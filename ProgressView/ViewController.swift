e1e111356746fadf2b4b00ba319059be40c1f257
A <tanaybhattacharjee@Tanays-MacBook-Air.local>
2018-04-14 21:53:48 +0530

    Initial Commit

---
+++ ProgressView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProgressView
+//
+//  Created by Tanay Bhattacharjee on 14/04/18.
+//  Copyright © 2018 Tanay Bhattacharjee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
